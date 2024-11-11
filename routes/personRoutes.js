7f4d923336f92fd639a5881712b9e49516b76747
amalieasborg <amalieasborg@gmail.com>
2024-11-11 15:27:19 +0100

    Router til personController

---
+++ routes/personRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Person = require('../models/person');
+const personController = require('../controllers/personController');
+
+// Route til visning af formularen for ny person
+router.get('/new', (req, res) => {
+    res.render('new');  // Render new.ejs
+});
+
+// Route to render form for editing an existing film
+router.get('/person/:id/update', async (req, res) => {
+    try {
+        const person = await Person.findById(req.params.id);
+        res.render('edit', { person: person });
+    } catch (err) {
+        console.error("Fejl ved hentning af person:", err);
+        res.status(500).send("Fejl ved hentning af person.");
+    }
+});
+
+router.get('/', personController.getAllPersons);
+router.post('/film', personController.createPerson);
+router.post('/film/:id/update', personController.updatePerson);
+router.post('/film/:id/delete', personController.deletePerson);
+
+module.exports = router
